8b9d6ec1fa3739d966c7567adb53f2b520ed8312
jowuoz <jowuoz254@gmail.com>
2026-01-05 22:42:19 +0300

    Update __init__.py

--- src/ notifier/__init__.py
+++ src/ notifier/__init__.py
@@ -1,1 +1,10 @@
+"""
+Notifier module - Alert and notification system
+"""
+from .telegram_client import TelegramNotifier
+from .alert_builder import AlertBuilder
 
+__all__ = [
+    'TelegramNotifier',
+    'AlertBuilder'
+]
