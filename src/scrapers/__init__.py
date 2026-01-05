a13e6d570e6aa3be1036a3fe8ae6db904e4da18d
jowuoz <jowuoz254@gmail.com>
2026-01-05 22:41:11 +0300

    Update __init__.py

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -1,1 +1,12 @@
+"""
+Scrapers module - Live data collection from various sources
+"""
+from .base_scraper import BaseScraper
+from .flashscore_scraper import FlashScoreScraper
+from .scraper_manager import ScraperManager
 
+__all__ = [
+    'BaseScraper',
+    'FlashScoreScraper',
+    'ScraperManager'
+]
